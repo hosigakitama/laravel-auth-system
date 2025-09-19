ff74b241b77094a6f7fbd2af8cd790c5b3e68a1d
bilalrafael <bilalrafaelyunanta@gmail.com>
2025-09-19 08:33:52 +0700

    crud blog done

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -11,4 +12,10 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function () {
         return auth()->user();
     });
+    Route::get('/blog', [BlogController::class, 'index']);
+    Route::get('/blog/{id}', [BlogController::class, 'show']);
+    Route::post('/blog', [BlogController::class, 'store']);
+    Route::put('/blog/{id}', [BlogController::class, 'update']);
+    Route::delete('/blog/{id}', [BlogController::class, 'destroy']);
 });
+
