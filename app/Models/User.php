bafa55d44363ac7b672e3d6e734c49995875d348
barabud <sahalbarabud5758@gmail.com>
2025-09-20 21:22:43 +0700

    crud event

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,4 +26,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function events()
+{
+    return $this->hasMany(Event::class, 'id_user');
 }
+
+}
+
